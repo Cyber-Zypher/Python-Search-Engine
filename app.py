dab00b2a4d919c99d0f97ca4b294447f63ae2dbd
Sidharth Everett <cyberzypher@gmail.com>
2023-11-04 17:37:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+
+app = Flask(__name__)
+
+# Configure the MySQL database connection
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='DB_NAME',
+    database='search_engine_db'
+)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    keyword = request.form['keyword']
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM search_results WHERE keyword LIKE %s", ('%' + keyword + '%',))
+    results = cursor.fetchall()
+    cursor.close()
+    return render_template('search_results.html', results=results)
+
+@app.route('/add_result', methods=['POST'])
+def add_result():
+    keyword = request.form['keyword']
+    result_text = request.form['result_text']
+
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO search_results (keyword, result_text) VALUES (%s, %s)", (keyword, result_text))
+    db.commit()
+    cursor.close()
+
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
